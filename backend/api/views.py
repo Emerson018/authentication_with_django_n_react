73831af6dce76dc27b86a2ac3ae89bce9795b09b
Emerson Viçosa <emersonvicosa@gmail.com>
2024-05-26 18:48:21 -0300

    authentication

---
+++ backend/api/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+from api.models import User
+from api.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = ([AllowAny])
+    serializer_class = RegisterSerializer
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def dashboard(request):
+    if request.method == "GET":
+        response = f"Eae {request.user}, o GET funcionou!"
+        return Response({'response': response}, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        text = request.POST.get("text")
+        response = f"Hey {request.user}, o texto é: {text}"
+        return Response({'response': response}, status=status.HTTP_200_OK)
+    
+    return Response({},status=status.HTTP_404_BAD_REQUEST)
+    
