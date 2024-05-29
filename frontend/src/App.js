4bc6a73fb44dfe3caa75d4f10d45a268f91a72b0
Emerson Viçosa <emersonvicosa@gmail.com>
2024-05-28 21:43:16 -0300

    consertando as rotas das views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import PrivateRoute from "./utils/PrivateRoute"
+import { AuthProvider} from "./context/AuthContext"
+
+import Homepage from  "./views/Homepage"
+import Registerpage from  "./views/Registerpage"
+import Loginpage from  "./views/Loginpage"
+import Dashboard from  "./views/Dashboard"
+import Navbar from  "./views/Navbar"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
 }
 
-export default App;
+export default App
