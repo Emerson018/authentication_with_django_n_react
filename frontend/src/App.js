e4dce24d949476615c37bbefb6730efdd0a41705
Emerson Viçosa <emersonvicosa@gmail.com>
2024-05-29 22:34:05 -0300

    arrumando route do appjs e add login e register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './views/Navbar'
 
-import { AuthProvider } from './context/AuthContext'
-
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './utils/PrivateRoute';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Dashboard from './views/Dashboard';
+import Loginpage from './views/Loginpage';
+import Registerpage from './views/Registerpage';
 
 function App() {
   return (
@@ -13,7 +17,10 @@ function App() {
       <AuthProvider>
         <Navbar />
         <Routes>
-     
+          
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/register" element={<Registerpage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}></Route>
         </Routes>
       </AuthProvider>
       
