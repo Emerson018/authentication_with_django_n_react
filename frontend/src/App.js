74b25e136bd59cf27c5a42664e031845366f1d44
Emerson Viçosa <emersonvicosa@gmail.com>
2024-05-30 23:42:39 -0300

    add homepage no app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,9 @@ import PrivateRoute from './utils/PrivateRoute';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Dashboard from './views/Dashboard';
 import Loginpage from './views/Loginpage';
 import Registerpage from './views/Registerpage';
+import Homepage from './views/Homepage';
 
 function App() {
   return (
@@ -20,7 +20,8 @@ function App() {
           
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
-        <Route path="/dashboard" element={<PrivateRoute />}></Route>
+        <Route path="/dashboard" element={<PrivateRoute />}/>
+        <Route path="/homepage" element={<Homepage />}/>
         </Routes>
       </AuthProvider>
       
